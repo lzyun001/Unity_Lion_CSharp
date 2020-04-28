8ecc800a09f4af5ef5a30ccf260fd1389b60f793
lzyun001 <63327467+lzyun001@users.noreply.github.com>
2020-04-28 14:41:32 +0800

    發布網頁版

---
+++ CSharp/Assets/Class3_Method.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Class3_Method : MonoBehaviour
+{
+    public Person person1;
+    public Person person2;
+    private void Start()
+    {
+        //呼叫 person1 方法
+        person1.Talk();
+        person2.Talk();
+
+        //傳回方法可以直接當作傳回類型使用
+        print("屁孩BMI:" + person1.BMI());
+        print("警察BMI:" + person2.BMI());
+        // 使用有參數的方法必須填入對應的引數
+        person1.Walk(99,sound:"嘎嘎嘎");
+        person2.Walk(10,"左邊");
+
+        person1.Attack();
+        person2.Attack("沙漠之鷹");
+    }
+}
---
+++ CSharp/Assets/Practice1_OperatorMethod.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Practice1_OperatorMethod : MonoBehaviour
+{
+    // 儲存兩筆數字
+    public string number1;
+    public string number2;
+
+    [Header("文字：輸出結果")]
+    public Text result;
+
+    /// <summary>
+    /// 給第一個輸入欄位使用
+    /// </summary>
+    /// <param name="number">第一個欄位的數值</param>
+    public void InputNumber1(string number)
+    { 
+        number1 = number;
+    }
+
+    public void InputNumber2(string number)
+    {
+        number2 = number;
+    }
+
+    public void Add()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+        
+        print("加法結果：" + (n1 + n2));
+
+        //輸出結果，文字內容 =
+        result.text = "加法結果：" + (n1 + n2);
+    }
+
+    public void Sub()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        result.text = "減法結果：" + (n1 - n2);
+    }
+    public void Mul()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+        result.text = "乘法結果：" + (n1 * n2);
+    }
+    public void Div()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+        result.text = "除法結果：" + (n1 / n2);
+    }
+    public void Rem()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+        result.text = "餘數：" + (n1 % n2);
+    }
+}
